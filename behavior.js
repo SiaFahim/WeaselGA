f8554e245e6cd6ffd4d8539cb5e3fe7f111b5ab1
SiaFahim <git@fahimi.me>
2015-11-13 10:40:24 -0800

    GitHub Card

--- behavior.js
+++ behavior.js
@@ -1,3 +1,25 @@
+
+
+// GitHub card script
+
+(function() {
+  $(document).ready(function() {
+
+    // Gets the full repo name from the data attribute and
+    // fetches the data from the api.
+    $('[data-gh-project]').each(function() {
+      var $proj = $(this);
+      var repo = $proj.data('gh-project');
+      $.get('https://api.github.com/repos/' + repo).success(function(data) {
+        $proj.find('.fork a').text(data.forks_count);
+        $proj.find('.star a').text(data.stargazers_count);
+      });
+    });
+
+  });
+})();
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
 var thumbnailElement = document.getElementById("smart_thumbnail");
